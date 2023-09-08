e6c0852179814c910a4f88ab19d2e737f0c19e09
Bleyle <Bennybleylle09@gmail.com>
2023-09-08 15:41:03 -0800

    Delete src directory

--- src/pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function hello(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'Hello Next.js 👋' });
-}
